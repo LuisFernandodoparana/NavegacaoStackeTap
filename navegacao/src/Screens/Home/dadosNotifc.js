6d9b01ad6e087a9b38122c3b5c748dde14711b73
Luis Fernando Santos Paulino <102885220+LuisFernandodoparana@users.noreply.github.com>
2022-10-04 21:07:54 -0300

    Create

---
+++ navegacao/src/Screens/Home/dadosNotifc.js
@@ -0,0 +1,17 @@
+export const notificationText = [
+    {
+        id: 1,
+        title: 'Se esta precisando de um eletricista!',
+        body: 'Não esqueça de conferir os nossos orçamentos para resindência e industria!'
+    },
+    {
+        id: 2,
+        title: 'Necessita de melhorias em suas instalações industriais!',
+        body: 'É abrir o nosso App e conferir os serviços que estão disponíveis!'
+    },
+    {
+        id: 3,
+        title: 'Aproveite o nosso desconto para novos clientes!',
+        body: 'E contrate um eletricista pela metade do preço!'
+    }
+]
